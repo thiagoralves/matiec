b082b44b62479aba0972bdc768d1116757c66049
Thiago Alves <thiagoralves@gmail.com>
2024-12-11 07:26:49 -0500

    Fix F_TRIG block

--- lib/C/iec_std_FB.h
+++ lib/C/iec_std_FB.h
@@ -598,7 +598,7 @@ static void F_TRIG_init__(F_TRIG *data__, BOOL retain) {
   __INIT_VAR(data__->ENO,__BOOL_LITERAL(TRUE),retain)
   __INIT_VAR(data__->CLK,__BOOL_LITERAL(FALSE),retain)
   __INIT_VAR(data__->Q,__BOOL_LITERAL(FALSE),retain)
-  __INIT_VAR(data__->M,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->M,__BOOL_LITERAL(TRUE),retain)
 }
 
 // Code part
